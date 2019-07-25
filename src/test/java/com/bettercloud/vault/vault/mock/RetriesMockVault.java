9763cfe553a18a1f333d2380bb2d83429a18db9a
Richard M <18526695+rimitchell@users.noreply.github.com>
2019-07-25 08:44:55 -0700

    chg: Updating RetryTests to support preflight call

--- src/test/java/com/bettercloud/vault/vault/mock/RetriesMockVault.java
+++ src/test/java/com/bettercloud/vault/vault/mock/RetriesMockVault.java
@@ -45,14 +45,18 @@ import java.io.IOException;
  */
 public class RetriesMockVault extends MockVault {
 
-    private int failureCount ;
+    private int failureCount;
+    private int retryCount;
     private int mockStatus;
     private String mockResponse;
+    private boolean handlePreflightQuery;
 
     public RetriesMockVault(final int failureCount, final int mockStatus, final String mockResponse) {
         this.failureCount = failureCount;
         this.mockStatus = mockStatus;
         this.mockResponse = mockResponse;
+        this.handlePreflightQuery = true;
+        this.retryCount = 0;
     }
 
     @Override
@@ -64,8 +68,13 @@ public class RetriesMockVault extends MockVault {
     ) throws IOException {
         response.setContentType("application/json");
         baseRequest.setHandled(true);
-        if (failureCount > 0) {
-            failureCount = failureCount - 1;
+        if (retryCount == 0) {
+            retryCount++;
+            response.setStatus(200); // This allows the prefligt response to work
+            System.out.println("RetriesMockVault is sending an HTTP 200 code, to allow the preflight check to pass");
+            response.getWriter().println("{\"request_id\": \"0\",  \"lease_id\": \"0\",  \"renewable\": false,  \"lease_duration\": 0,  \"data\": { \"accessor\": \"0\",    \"config\": {      \"default_lease_ttl\": 0,      \"force_no_cache\": false,      \"max_lease_ttl\": 0    },    \"description\": \"\",    \"local\": false,    \"options\": {      \"version\": \"2\"    },    \"path\": \"secret\",    \"seal_wrap\": false,    \"type\": \"kv\"  }}");
+        } else if (retryCount <= failureCount) {
+            retryCount++;
             response.setStatus(500);
             System.out.println("RetriesMockVault is sending an HTTP 500 code, to cause a retry...");
         } else {
